09af9fec926d07bc1ae12ce7bb0fec0c72336906
Nikita Fendel <fendelnikita@gmail.com>
2024-03-20 00:15:06 +0600

    feat: prepare folders

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Battleground from "./pages/battleground/Battleground";
 import PlacementShips from "./pages/placement-ships/PlacementShips";
-import TestPage from "./pages/testpages/TestPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="testpage" element={<TestPage index={5} />} />
         <Route path="battleground" element={<Battleground />} />
         <Route path="placementships" element={<PlacementShips />} />
       </Routes>
